1fbedebc9ef2547c7ea0b3a7f042a92fe20c140c
dassoumyad <sd83580@gmail.com>
2026-08-26 22:15:31 +0700

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -12,29 +12,53 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// ===============================
+// CORS
+// ===============================
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://convertbot-crp0.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
+// ===============================
 // Middleware
+// ===============================
+
 app.use(express.json());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-
+// ===============================
 // Connect MongoDB
+// ===============================
+
 connectDB();
 
+// ===============================
 // Routes
+// ===============================
+
 app.use("/api/auth", authRoutes);
 app.use("/api/ai", aiRoutes);
 
-// Test route
+// ===============================
+// Test Route
+// ===============================
+
 app.get("/", (req, res) => {
   res.json({
     message: "Server is working",
   });
 });
 
-// Start server
+// ===============================
+// Start Server
+// ===============================
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
