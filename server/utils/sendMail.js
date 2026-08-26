fb1a4cf545765da24fefc13f318cfd67483edb9a
dassoumyad <sd83580@gmail.com>
2026-08-26 23:09:57 +0700

    Use change to email and password

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -2,12 +2,14 @@ const nodemailer = require("nodemailer");
 
 const sendMail = async (options) => {
   try {
-    // Check email credentials
-    if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
+
+    if (
+      !process.env.EMAIL_USER ||
+      !process.env.EMAIL_PASSWORD
+    ) {
       throw new Error("Email credentials not found");
     }
 
-    // Create Gmail transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
 
@@ -17,24 +19,20 @@ const sendMail = async (options) => {
       },
     });
 
-    // Email details
     const mailOptions = {
       from: process.env.EMAIL_USER,
-
       to: options.email,
-
       subject: options.subject,
-
       text: options.message,
-
       html: `<p><b>${options.message}</b></p>`,
     };
 
-    // Send email
     await transporter.sendMail(mailOptions);
 
     console.log("Email sent successfully");
+
   } catch (error) {
+
     console.log("Email error:", error.message);
 
     throw error;
